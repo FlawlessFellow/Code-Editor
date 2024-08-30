5ea96fe9391c89d6b054f6cfba93def950d1e5b1
FlawlessFellow <flawlesselish@gmail.com>
2024-08-30 13:21:17 +0200

    Refactored code, added 'CodeCell' component, and new library 'react-resizable@3.0.4

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,5 +1,32 @@
+import { useState } from 'react';
+import CodeEditor from './code-editor';
+import Preview from './preview';
+import bundle from '../bundler';
+
 const CodeCell = () => {
-    return <div></div>;
+    const [code, setCode] = useState('');
+    const [input, setInput] = useState('');
+
+    const onClick = async () => {
+        const output = await bundle(input);
+
+        setCode(output);
+    };
+
+    return (
+        <>
+            <CodeEditor initialValue="const a = 1;" onChange={(value) => setInput(value)} />
+            {/* <textarea
+                style={{ width: '500px', height: '200px' }}
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+            ></textarea> */}
+            <div>
+                <button onClick={onClick}>Submit</button>
+            </div>
+            <Preview code={code} />
+        </>
+    );
 };
 
 export default CodeCell;
