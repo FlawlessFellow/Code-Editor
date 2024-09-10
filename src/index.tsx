2da7b15b4652ee85e3bd1d84fa2ae00194160a1c
FlawlessFellow <flawlesselish@gmail.com>
2024-09-10 22:09:02 +0200

    Added new components and made adjustments to the Redux state

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,17 @@ import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './state';
+import CellList from './components/cell-list/cell-list';
 // import CodeCell from './components/code-cell/code-cell';
-import TextEditor from './components/text-editor/text-editor';
+// import TextEditor from './components/text-editor/text-editor';
 
 const App = () => {
     return (
         <Provider store={store}>
             <div>
                 {/* <CodeCell /> */}
-                <TextEditor />
+                {/* <TextEditor /> */}
+                <CellList />
             </div>
         </Provider>
     );
