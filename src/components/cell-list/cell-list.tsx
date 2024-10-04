855b9a6799e7458b983d1835df503b9ff891a5bf
FlawlessFellow <flawlesselish@gmail.com>
2024-10-04 14:48:24 +0200

    Styling add-cell component

--- src/components/cell-list/cell-list.tsx
+++ src/components/cell-list/cell-list.tsx
@@ -1,12 +1,24 @@
+import { Fragment } from 'react';
 import { useTypedSelector } from '../../hooks/use-typed-selector';
 import CellListItem from '../cell-list-item/cell-list-item';
+import AddCell from '../add-cell/add-cell';
 
 const CellList: React.FC = () => {
     const cells = useTypedSelector(({ cells: { order, data } }) => order.map((id) => data[id]));
 
-    const renderedCells = cells.map((cell) => <CellListItem key={cell.id} cell={cell}></CellListItem>);
+    const renderedCells = cells.map((cell) => (
+        <Fragment key={cell.id}>
+            <AddCell nextCellId={cell.id} />
+            <CellListItem key={cell.id} cell={cell}></CellListItem>
+        </Fragment>
+    ));
 
-    return <div>{renderedCells}</div>;
+    return (
+        <div>
+            {renderedCells}
+            <AddCell nextCellId={null} />
+        </div>
+    );
 };
 
 export default CellList;
